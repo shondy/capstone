0cd6a08d204f51fcaf8d2d2a736596f7b1fe280f
Shondy <olga.tarasova@gmail.com>
2022-02-26 19:41:54 -0800

    change database uri from postgres to postgresql

--- config_app.py
+++ config_app.py
@@ -6,7 +6,12 @@ class Config(object):
     DEBUG = False
     TESTING = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default_sekret')
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    uri = os.getenv("DATABASE_URL")  # or other relevant config var
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = uri
+    # rest of connection code using the connection string `uri`
 
 
 class ProductionConfig(Config):
