cfbd8004b0246067381e2ce20cd67901b60b79b3
Shondy <olga.tarasova@gmail.com>
2022-03-20 16:50:20 -0700

    env var in setup.sh3

--- capstone/auth.py
+++ capstone/auth.py
@@ -9,6 +9,7 @@ from urllib.request import urlopen
 AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
 ALGORITHMS = [os.environ.get('ALGORITHMS')]
 API_AUDIENCE = os.environ.get('API_AUDIENCE')
+print(AUTH0_DOMAIN, ALGORITHMS, API_AUDIENCE)
 
 
 ## AuthError Exception
